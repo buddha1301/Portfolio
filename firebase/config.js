a99e628e806e932fc13e89c9ca48e13c7d881dcc
buddha1301 <buddhaj_rai.srmus.edu.in>
2023-06-28 14:54:27 +0530

    firebase config added

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC_Q0bbxomPK1XpUnmw9t1JaOBbk1JPt0Y",
+    authDomain: "oldagevoterslist.firebaseapp.com",
+    projectId: "oldagevoterslist",
+    storageBucket: "oldagevoterslist.appspot.com",
+    messagingSenderId: "1026564943014",
+    appId: "1:1026564943014:web:b7a86a4736558ebbb79867"
+  };
