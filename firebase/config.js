db4423f0c1b29e54f93091c12b4f8323a8a640a9
buddha1301 <buddhaj_rai.srmus.edu.in>
2023-07-03 15:55:23 +0530

    added form

--- firebase/config.js
+++ firebase/config.js
@@ -1,4 +1,8 @@
 // Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getFirestore,doc,setDoc } from "firebase/firestore";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyC_Q0bbxomPK1XpUnmw9t1JaOBbk1JPt0Y",
     authDomain: "oldagevoterslist.firebaseapp.com",
@@ -7,3 +11,13 @@ const firebaseConfig = {
     messagingSenderId: "1026564943014",
     appId: "1:1026564943014:web:b7a86a4736558ebbb79867"
   };
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export {doc,setDoc}
+
+
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import {doc,setDoc} from "./firebase/config"
 
 document.querySelector('#app').innerHTML = `
   <div>
