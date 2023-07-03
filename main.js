d731f46981fba48f76f66cebd094acf6bcbdbe0d
buddha1301 <buddhaj_rai.srmus.edu.in>
2023-07-03 15:20:45 +0530

    added form

--- main.js
+++ main.js
@@ -11,13 +11,39 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Online Voting System</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
+
+    <form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br>
+  <label for "address">Address:</label><br>
+  <input type="text" id="address" name="address"> <br>
+  <label for="voter_id"> voter_id:</label><br>
+  <input type="text" id="voter_id" name="voter_id"><br>
+  <label for="word_no"> word_no:</label><br>
+  <input type="word_no" id = "word_number" name="word_number"<br><br><br>
+  <label for="district">Choose a district:</label>
+  <select name="district" id="district">
+    <option value="gangtok">gangtok</option>
+    <option value="pakyoung">pakyoung</option>
+    <option value="soreng">soreng</option>
+    <option value="magan">magan</option>
+    <option value="namchi">namchi</option>
+    <option value="geyzing">geyzing</option>
+  </select>
+    <br><br>
+  <input type="submit" value="Submit">
+</form>
+
+
   </div>
 `
 
